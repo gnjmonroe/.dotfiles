84f9b5acff2aa9279574bdcce7311752f9da64b6
Greg Monroe <gnjmonroe@gmail.com>
2024-03-02 20:52:10 +0900

    add gh cli and pnpm

--- install.sh
+++ install.sh
@@ -14,3 +14,12 @@ $ sudo apt-get install ripgrep
 # install nvchad
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
 
+# install gh cli
+sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
+# install pnpm
+curl -fsSL https://get.pnpm.io/install.sh | sh -
